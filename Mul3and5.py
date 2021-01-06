b6c991fffc6d6a5f243e9ae021b5b04f8e83dd24
Raul <raul19@windowslive.com>
2021-01-06 16:05:35 +0200

    Create Mul3and5.py

---
+++ Mul3and5.py
@@ -0,0 +1,20 @@
+#!usr/bin/python3
+
+# Multiples of 5 and 3 
+
+def mul(number1,number2):
+    sum3=0
+    sum5=0
+    sum15=0
+    for i in range(1,334):
+            sum3+=i*number1
+            
+    for i in range(1,200):
+            sum5+=i*number2 
+            
+    for i in range(1,67):
+            sum15+=i*number2*3         
+    
+    return sum3+sum5-sum15
+
+print(mul(3,5))
